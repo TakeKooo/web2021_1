1f57e0d793fda3d4dd6de43abd5e59838f5e5435
take-kkp <136552463+take-kkp@users.noreply.github.com>
2023-06-21 05:45:37 +0000

    dbへの追加を記述

--- app6.js
+++ app6.js
@@ -6,6 +6,8 @@ const db = new sqlite3.Database('test.db');
 
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   const message = "Hello world";
@@ -50,6 +52,24 @@ app.get("/top", (req, res) => {
     })
   })
 })
+
+app.post("/insert", (req, res) => {
+  let sql = `
+insert into example (都道府県,人口,大学) values ("` + req.body.name + `",` + req.body.jinko + `,` + req.body.daigaku + `);
+`
+  console.log(sql);
+  db.serialize(() => {
+    db.run(sql, (error, row) => {
+      console.log(error);
+      if (error) {
+        res.render('show', { mes: "エラーです" });
+      }
+      res.redirect('/db');
+    });
+  });
+  console.log(req.body);
+});
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
